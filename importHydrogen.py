7825d95c67ff92737c421ab2b26c7ed9e29ae71a
CorentinDupont <93649468+CorentinSauteDupont@users.noreply.github.com>
2025-02-14 15:55:58 +0100

    Update importHydrogen.py

--- importHydrogen.py
+++ importHydrogen.py
@@ -4,28 +4,24 @@ import pyomo.environ as pyo
 # Create a Pyomo model
 model = pyo.ConcreteModel()
 
+
 # Define model parameters
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.boat_capacity = pyo.Param
 
 # Define model variables
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.Nbr_boats_CH4 = pyo.Var(within = pyo.NonNegativeReals)
+model.Nbr_boats_NH3 = pyo.Var(within = pyo.NonNegativeReals) 
+
 
 # Define the objective functions
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.obj = pyo.Objective(expr = 2*model. × 1 + 4*model. × 2, sense = pyo.maximize) 
 
 # Define the constraints
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+Nbr_boats_CH4 + Nbr_boats_NH3 = 100
+
 
 # Specify the path towards your solver (gurobi) file
-solver = pyo.SolverFactory('...')
+solver = pyo.SolverFactory('C:\Users\dupti\gurobi.lic')
 sol = solver.solve(model)
 
 # Print here the number of CH4 boats and NH3 boats
